90fc7b440000e93d37e46f915b1bed275eb79152
Salb213 <salb213is.on@gmail.com>
2025-08-06 15:45:14 +0200

    Fix tokenizer import issue and add .env support

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -1,1 +1,12 @@
-# TODO: Write code to load and tokenize text data
+from tokenizer.tokenizer import CharTokenizer
+
+file_path = "data/raw/input.txt"
+
+with open(file_path, "r", encoding="utf-8") as f:
+    text = f.read()
+
+print("First 100 Chars:\n", text[:100])
+
+tokenizer = CharTokenizer(text)
+
+data = tokenizer.encoded(text)
